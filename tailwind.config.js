3800a683cf3bfdcc38bcc90c22f2549ea9c74364
sormiston <sean.david.ormiston@gmail.com>
2023-05-12 16:58:00 +0100

    ready to create section schema: responsive nav bar

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './templates/*.liquid',
+    './templates/customers/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
